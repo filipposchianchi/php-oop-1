a68dfd350176604e7ef7ba46a06bbddaf46c9bff
filippo <schianchi.filippo.93@gmail.com>
2020-01-09 15:56:39 +0100

    finito esercizio

--- todoToday.php
+++ todoToday.php
@@ -28,45 +28,47 @@ class Configurazione {
 
 // connessione al DB
 
-$server = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "HotelDB";
-
-$conn = new mysqli($server, $username, $password, $dbname);
-if ($conn -> connect_errno) {
-
-  echo json_encode(-1);
-  return;
-}
 
 // download di tutte le configurazioni
 
-$sql = "
+  //header('Content-Type: application/json');
 
-    SELECT *
-    FROM configurazioni
+  $server = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "HotelDB";
 
-";
+  $conn = new mysqli($server, $username, $password, $dbname);
+  if ($conn -> connect_errno) {
 
-$res = $conn -> query($sql);
-if ($res -> num_rows < 1) {
+    echo json_encode(-1);
+    return;
+  }
 
-  echo json_encode(-2);
-  return;
-}
+  $sql = "
+
+      SELECT *
+      FROM configurazioni
+
+  ";
+
+  $res = $conn -> query($sql);
+  if ($res -> num_rows < 1) {
+
+    echo json_encode(-2);
+    return;
+  }
+
 
 $confs = [];
-while ($conf = $res->fetch_assoc()) {
+while($conf = $res -> fetch_assoc()) {
 
-  $myConf = new Configurazione(
-    $conf['id'],
-    $conf['title'],
-    $conf['description']
-  );
-  $confs[] = $myConf;
+  $confs = new Configurazione($conf['id'],
+                              $conf['title'],
+                              $conf['description']);
 }
 
-foreach ($confs as $key) {
-  echo '<pre>' . $key -> __toString()  . PHP_EOL . '</pre>';
+foreach ($confs as $conf) {
+
+  echo $conf;
 }
